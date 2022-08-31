ddcad26daf5a8978df0bb5192d763de24df7fc86
Ivan Potapenko <dreamer4ik@gmail.com>
2022-08-31 20:56:47 +0300

    Initial Commit

---
+++ TinderSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderSwift
+//
+//  Created by Ivan Potapenko on 31.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
